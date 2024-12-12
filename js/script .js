235efeb010178d5354d46d6588a94ab0d64f3fc0
Soto bu arya <aryamah2@gmail.com>
2024-12-12 18:50:10 +0700

    memperbaiki

--- js/script .js
+++ js/script .js
@@ -1,11 +1,5 @@
 // Toggle class active
 const navbarNav = document.querySelector(".navbar-extra");
-document.querySelector("#makanan-menu").oneclick = () => {
-  navbarNav.classList.toggle("active");
-};
-// JavaScript
-// Mendapatkan elemen navbar dan tombol
-const navbarNav = document.querySelector(".navbar-extra");
 const menuButton = document.querySelector("#makanan-menu");
 
 // Menambahkan event listener klik ke tombol
